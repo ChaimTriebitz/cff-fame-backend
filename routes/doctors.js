375bb2f29d74d1ac0df06135893b5905f633b78e
chaim <chaim.triebitz@bhbsdventures.com>
2024-10-15 18:17:01 +0300

    sucot

--- routes/doctors.js
+++ routes/doctors.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Doctors = require('../models/Doctors');
 
-// Create an Doctor
+
 router.post('/', async (req, res) => {
 
    const {
